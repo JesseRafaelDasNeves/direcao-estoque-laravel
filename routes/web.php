659639c4ba6a6405dff44aa3244f704398ac29a1
JesseRafaelDasNeves <jesserafael@hotmail.com.br>
2019-08-25 09:07:19 -0300

    rote-del-welcome
    Retorado rota e view wecome pardrão do laravel

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
+Route::get('/'    , 'HomeController@index')->name('home');
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
